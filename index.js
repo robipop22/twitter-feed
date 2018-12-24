6de2bcb682e7e9ce9a8be77a672a355247673c41
Robert Pop <poprobertdaniel22@gmail.com>
2018-12-24 13:06:38 +0200

    changed now env

--- index.js
+++ index.js
@@ -4,11 +4,17 @@ const app = express()
 
 const port = 8000
 
-app.use(bodyParser.json())
+require('now-env')
 
-app.listen(port, () => {
-  console.log('We are live on ' + port)
+var Twitter = require('twitter')
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 })
+
+app.use(bodyParser.json())
 
 app.get('/timeline', (req, res) => {
   var params = { user_id: req.query.user_id }
@@ -24,11 +30,6 @@ app.get('/timeline', (req, res) => {
   })
 })
 
-require('dotenv').config()
-var Twitter = require('twitter')
-var client = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+app.listen(port, () => {
+  console.log('We are live on ' + port)
 })
