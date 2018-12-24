4a0b001d9da4824e8251a218372db2430f02b20a
Robert Pop <poprobertdaniel22@gmail.com>
2018-12-24 12:17:35 +0200

    working timeline

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+const port = 8000
+
+app.use(bodyParser.json())
+
+app.listen(port, () => {
+  console.log('We are live on ' + port)
+})
+
+app.get('/timeline', (req, res) => {
+  var params = { user_id: req.query.user_id }
+
+  client.get('statuses/user_timeline', params, function(
+    error,
+    tweets,
+    response
+  ) {
+    if (!error) {
+      res.send({ tweets })
+    }
+  })
+})
+
 require('dotenv').config()
 var Twitter = require('twitter')
 var client = new Twitter({
@@ -5,11 +31,4 @@ var client = new Twitter({
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-})
-
-var params = { user_id: process.env.TWITTER_USER_ID }
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets)
-  }
 })
