81860330a8abf3ca443ff21d6643aaea98ab8328
Robert Pop <poprobertdaniel22@gmail.com>
2018-12-24 13:24:45 +0200

    origin

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ var client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 })
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
+
 app.use(bodyParser.json())
 
 app.get('/timeline', (req, res) => {
